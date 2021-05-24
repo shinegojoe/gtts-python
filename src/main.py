7565b6c8baae24877287e6ee70e9bdb3c9331783
taka <taka@bxb.tw>
2021-05-24 09:20:47 +0800

    add translate api

--- src/main.py
+++ src/main.py
@@ -3,10 +3,15 @@
 from dotenv import dotenv_values
 from gtts import gTTS 
 from flask import Flask, jsonify, request
+from translate import Translator
+
 
 config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 port = int(config["PORT"])
 tempDir = config["TEMP_DIR"]
+
+# port = 7001
+# tempDir = '/app/storage'
 app = Flask(__name__)
 
 
@@ -21,6 +26,21 @@ def ggts():
         output = '{}/{}'.format(tempDir, "output.mp3")
         myobj.save(output)
         res = jsonify({"status": "ok"})
+    except Exception as e:
+        res = jsonify({"status": "err"})
+
+    return res
+
+@app.route("/translate", methods=['POST'])
+def test():
+    try:
+        data = request.json
+        text = data['text']
+        to_lang = 'zh-TW'
+        translator= Translator(to_lang=to_lang)
+        res = translator.translate(text)
+        res = jsonify({"status": "ok", "text": res})
+
     except Exception as e:
         res = jsonify({"status": "err"})
 
